f0ca2a12f3d4da0a788235d34faff3bf430edc9f
Bruno Ricardo Secco <brunodvic3@hotmail.com>
2019-09-24 16:18:33 -0300

    encoder

--- caesarCipher.js
+++ caesarCipher.js
@@ -32,27 +32,3 @@ function encode(phrase) {
   console.log(cipherWord);
   return cipherWord;
 }
-
-function decode(phrase) {
-  let cipherWord = "";
-  const vectorWord = phrase.toLowerCase().split("");
-
-  for (var i = 0; i < vectorWord.length; i++) {
-    if (!/\s/g.test(vectorWord[i])) {
-      for (var j = 0; j < vectorAlphabet.length; j++) {
-        if (vectorWord[i] == vectorAlphabet[j]) {
-          if (j - LETTER_JUMP < 0) {
-            let nextLap = j - LETTER_JUMP + vectorAlphabet.length;
-            cipherWord += vectorAlphabet[nextLap];
-          } else {
-            cipherWord += vectorAlphabet[j - LETTER_JUMP];
-          }
-          break;
-        }
-      }
-    } else {
-      cipherWord += " ";
-    }
-  }
-  console.log(cipherWord);
-}
